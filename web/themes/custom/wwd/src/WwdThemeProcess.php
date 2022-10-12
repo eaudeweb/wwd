8be41b9a0853e03f82a47d264c5e44d287dbe9da
Nick Procopan <nick@optasy.com>
2022-10-12 15:14:09 +0300

    Enable layout builder translations, rework banner region

--- web/themes/custom/wwd/src/WwdThemeProcess.php
+++ web/themes/custom/wwd/src/WwdThemeProcess.php
@@ -3,7 +3,10 @@
 namespace Drupal\wwd;
 
 use Drupal\views\Views;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
 use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
@@ -32,18 +35,29 @@ class WwdThemeProcess implements ContainerInjectionInterface {
   protected $languageManager;
 
   /**
+   * Current route match.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
    * Constructs a new EntityOperations object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Entity type manager service.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager service.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   Current route match.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
-    LanguageManagerInterface $language_manager) {
+    LanguageManagerInterface $language_manager,
+    CurrentRouteMatch $route_match) {
     $this->entityTypeManager = $entity_type_manager;
     $this->languageManager = $language_manager;
+    $this->routeMatch = $route_match;
   }
 
   /**
@@ -52,7 +66,8 @@ class WwdThemeProcess implements ContainerInjectionInterface {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('language_manager')
+      $container->get('language_manager'),
+      $container->get('current_route_match')
     );
   }
 
@@ -159,6 +174,58 @@ class WwdThemeProcess implements ContainerInjectionInterface {
           $gradientOptions .= $variables['second_color'];
 
           $variables['gradient_options'] = $gradientOptions;
+        }
+      }
+    }
+  }
+
+  /**
+   * Preprocess html.
+   */
+  public function preprocessHtml(&$variables) {
+    $node = $this->routeMatch->getParameter('node');
+    if ($node instanceof Node) {
+      if ($node->hasField('field_add_banner') &&
+        $node->get('field_add_banner')->value == 1) {
+        $variables['attributes']['class'][] = 'use-banner';
+      }
+    }
+  }
+
+  /**
+   * Preprocess page.
+   */
+  public function preprocessPage(&$variables) {
+    $node = $variables['node'];
+    if ($node instanceof NodeInterface) {
+      if ($node->getType() == 'page') {
+        if ($node->get('field_add_banner')->value == 1) {
+          $variables['use_banner'] = TRUE;
+        }
+        // Retrieve gradients if any.
+        if ($node->get('field_use_gradient')->value == 1) {
+          $variables['use_gradient'] = TRUE;
+          if (!$node->get('field_background_gradient')->isEmpty()) {
+            $paragraph = $node->get('field_background_gradient')->entity;
+            if ($paragraph instanceof Paragraph && $paragraph->bundle() == 'background_gradient') {
+              $variables['background_gradient'] = $this->entityTypeManager->getViewBuilder('paragraph')
+                ->view($paragraph);
+            }
+          }
+
+        }
+        // Retrieve banner title.
+        $bannerTitle = $node->get('field_banner_title')->getString();
+        $variables['banner_title'] = $bannerTitle !== '' ? $bannerTitle : $node->getTitle();
+        // Set background.
+        $variables['has_video'] = FALSE;
+        if (!$node->get('field_media_image')->isEmpty()) {
+          $media = $node->get('field_media_image')->entity;
+          if ($media->bundle() == 'video') {
+            $variables['has_video'] = TRUE;
+          }
+          $variables['media_url'] = $this->entityTypeManager->getViewBuilder('media')
+            ->view($media, 'background');
         }
       }
     }
