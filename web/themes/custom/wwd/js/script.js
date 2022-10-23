27ac214321f6cc7d3feefe4b9a05442b812cf1d2
Max <max@selk.global>
2022-10-23 09:52:31 +0300

    Fixed Header, Search icon, scroll issues

--- web/themes/custom/wwd/js/script.js
+++ web/themes/custom/wwd/js/script.js
@@ -3,24 +3,9 @@
  * Description.
 */
 
-(function ($, Drupal, once) {
-  Drupal.behaviors.general = {
-    attach: function (context) {
-    }
-  };
-})(jQuery, Drupal, once);
-
-let scrollpos = window.scrollY
-const header = document.getElementById("header")
-
-window.addEventListener('scroll', function() {
-  scrollpos = window.scrollY;
-
-  if (scrollpos >= 50) {
-    header.classList.remove("bg-tr")
-    header.classList.add(...["h-sticky", "shadow"])
-  } else {
-    header.classList.add("bg-tr")
-    header.classList.remove(...["h-sticky", "shadow"])
-  }
-})
+// (function ($, Drupal, once) {
+//   Drupal.behaviors.general = {
+//     attach: function (context) {
+//     }
+//   };
+// })(jQuery, Drupal, once);
